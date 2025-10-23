9b17383199f07986d5827e713473c5ec5f3a3704
Aiden Voth <ajv7509@rit.edu>
2025-10-22 21:00:43 -0400

    default sim settings

--- simulation.c
+++ simulation.c
@@ -1,2 +1,15 @@
 #include "simulation.h"
 #include "display.h"
+
+SimulationSettings make_default() {
+  SimulationSettings settings;
+
+  settings.initial_burning = 0.1;
+  settings.fire_probability = 0.3;
+  settings.forest_density = 0.5;
+  settings.neighbor_effect = 0.25;
+  settings.grid_size = 10;
+  settings.print_mode = OVERLAY;
+
+  return settings;
+}
