304830ebb1455a1c2832f4cce140e6aecf05d9f9
Aiden Voth <ajv7509@rit.edu>
2025-10-22 21:43:37 -0400

    base parsing program

--- wildfire.c
+++ wildfire.c
@@ -1,4 +1,14 @@
+#include <stdlib.h>
+
 #include "args.h"
 #include "simulation.h"
 
-int main(void) {}
+int main(int argc, char *argv[]) {
+    SimulationSettings settings = make_default();
+
+    if (!update_settings(&settings, argc, argv)) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
